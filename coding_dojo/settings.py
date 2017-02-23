307f7ca622588f749f76a5ca2abf3f2d3898533d
Ubuntu <ubuntu@ip-172-31-37-122.us-west-2.compute.internal>
2017-02-23 19:01:03 +0000

    courses un-enabled

--- coding_dojo/settings.py
+++ coding_dojo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!$uv^&kyy(1@7y-&193i*=i3ptpcyfg3$n^-75$!vc!%e$kp=m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.41.61.18']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
